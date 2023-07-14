fd588b756d7df41afb80012cc7b4a2850d79ed66
Guy Kabilio <guyka00@gmail.com>
2023-07-14 03:57:11 +0300

    styling

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -11,8 +11,8 @@ const Discover: NextPage = () => {
   const searchParams = useSearchParams()
   const topic = searchParams.get('topic')
    
-  const activeTopicStyle = 'xl:border-2 hover:bg-primary xl:border-[#F51997] px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-[#F51997]';
-  const topicStyle = 'xl:border-2 hover:bg-primary xl:border-gray-300 px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-black';
+  const activeTopicStyle = 'xl:border-2 hover:bg-primary xl:border-[#F51997] px-3 py-2 rounded-lg xl:rounded-lg flex items-center gap-2 justify-center cursor-pointer text-[#F51997]';
+  const topicStyle = 'xl:border-2 hover:bg-primary xl:border-gray-300 px-3 py-2 rounded-lg xl:rounded-lg flex items-center gap-2 justify-center cursor-pointer text-black';
 
   return (
     <div className='xl:border-b-2 xl:border-gray-200 pb-6'>
