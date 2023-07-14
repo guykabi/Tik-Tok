786f7b18b116d63fec0beb1edf8ce5e2c1c98295
Guy Kabilio <guyka00@gmail.com>
2023-07-14 03:54:03 +0300

    dev & prod url

--- app/api/api-instance.js
+++ app/api/api-instance.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const Axios = axios.create({
     withCredentials: true,
-    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL
+    baseURL: process.env.NODE_ENV === 'production'? 
+        process.env.NEXT_PUBLIC_BACKEND_URL_PROD :
+        process.env.NEXT_PUBLIC_BACKEND_URL
 });
 
 export default Axios;
