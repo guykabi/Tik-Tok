c4a4448a4be0b29ccfca80708d51142101a039f3
Guy Kabilio <guyka00@gmail.com>
2023-07-15 19:53:49 +0300

    styling

--- components/Suggestations.tsx
+++ components/Suggestations.tsx
@@ -29,7 +29,7 @@ const SuggestedAccounts: NextPage<IProps> = ({ fetchAllUsers, allUsers }) => {
       <div>
         {users?.slice(0, 6).map((user: IUser) => (
           <Link href={`/profile/${user._id}`} key={user._id}>
-            <div className='flex gap-3 hover:bg-primary p-2 cursor-pointer font-semibold rounded'>
+            <div className='flex gap-3 hover:bg-white p-2 cursor-pointer font-semibold rounded'>
               <div className='w-8 h-8'>
                 <Image
                   width={34}
