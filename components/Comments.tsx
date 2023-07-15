ef6c83eaceda9c864e167a6c6a21c17b43ac8820
Guy Kabilio <guyka00@gmail.com>
2023-07-15 13:42:13 +0300

    removing scrolling bars

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -28,7 +28,7 @@ const Comments = ({ comment, setComment, addComment, comments, isPostingComment 
 
   return (
     <div className='border-t-2 border-gray-200 pt-4 px-10 mt-4 bg-[#F8F8F8] border-b-2 lg:pb-0 pb-[100px]'>
-      <div className='overflow-scroll lg:h-[457px]'>
+      <div className='overflow-scroll no-scrollbar lg:h-[457px]'>
         {comments?.length > 0 ? (
           comments?.map((item: IComment, idx: number) => (
             <div key={idx}>
@@ -73,7 +73,7 @@ const Comments = ({ comment, setComment, addComment, comments, isPostingComment 
           <input
             value={comment}
             onChange={(e) => setComment(e.target.value)}
-            className='bg-primary px-6 py-4 text-md font-medium border-2 w-[250px] md:w-[700px] lg:w-[350px] border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 flex-1 rounded-lg'
+            className='bg-primary px-6 py-4 text-md font-medium border-2 w-[250px] md:w-9/12 lg:w-[350px] border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 flex-1 rounded-lg'
             placeholder='Add comment..'
           />
           <Button 
@@ -81,7 +81,7 @@ const Comments = ({ comment, setComment, addComment, comments, isPostingComment 
           text={isPostingComment ? 'Commenting...' : 'Comment'}
           color='blue'
           textColor='white'
-          width='8'
+          width='6'
           disable={comment?.length==0}/>
             
         </form>
